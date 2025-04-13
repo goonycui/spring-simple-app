746e07496974ddb6c7439af22825e183d56791cb
goonycui <goonycui@outlool.com>
2025-04-13 22:47:17 +0800

    enhance app to expose one more api

--- src/main/java/com/example/spring_simple_app/controller/TestController.java
+++ src/main/java/com/example/spring_simple_app/controller/TestController.java
@@ -4,17 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Autowired
+    Environment environment;
 
     @GetMapping("/spring")
     public Response test() {
         return Response.builder()
                 .code("200")
                 .message("helle from spring simple app")
+                .build();
+    }
+
+    @GetMapping("/env/{key}")
+    public Response env(@PathVariable String key) {
+        return Response.builder()
+                .code("200")
+                .message("env value for " + key + " is " + environment.getProperty(key, "default"))
                 .build();
     }
 
